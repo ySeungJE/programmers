97613383e5691d18ba0de619afa3d5d8e7bcdb47
ySeungJE <dbstmdwp98@naver.com>
2024-04-14 20:21:42 +0900

    240414 commit - 가장 많이 받은 선물

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public int solution(String[] friends, String[] gifts) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < friends.length; i++) {
+            map.put(friends[i], i);
+        }
+        int[] index = new int[friends.length];
+        int[][] record = new int[friends.length][friends.length];
+
+        for (String str : gifts) {
+            String[] cur = str.split(" ");
+            index[map.get(cur[0])]++;
+            index[map.get(cur[1])]--;
+            record[map.get(cur[0])][map.get(cur[1])]++;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < friends.length; i++) {
+            int cnt = 0;
+            for (int j = 0; j < friends.length; j++) {
+                if(i == j) continue;
+                if (record[i][j] > record[j][i]) cnt++;
+                else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++;
+            }
+            ans = Math.max(cnt, ans);
+        }
+        return ans;
     }
 }
